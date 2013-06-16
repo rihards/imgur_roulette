9e3db1e8a71ed4e97ae40e8bc156165c364912f2
Rihards Steinbergs <rihards@exove.fi>
2013-06-16 16:22:38 +0300

    Changed to google CDN jquery and the URL of the logo

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.2.0/pure-min.css">
 <link rel="stylesheet" href="assets/css/app.css">
-<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
 <script type="text/javascript" src="assets/js/imagesloaded.pkgd.min.js"></script>
 <script type="text/javascript" src="assets/js/masonry.pkgd.min.js"></script>
 </head>
@@ -15,7 +15,7 @@
 <div class="pure-g-r">
 	<div class="header pure-u-1">
 		<div class="pure-menu pure-menu-open pure-menu-horizontal">
-			<a class="pure-menu-heading" href="/">Imgur Roulette</a>
+			<a class="pure-menu-heading" href="<?php echo $_SERVER["PHP_SELF"]; ?>">Imgur Roulette</a>
 			<ul>
 				<li><a id="loadmore" href="#">Load more</a></li>
 			</ul>
